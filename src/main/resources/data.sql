471a7cae954f847e00b145a4fef133c292ac3848
Platon73 <platonova_0@list.ru>
2022-03-09 22:56:46 +0400

    добавил файл data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- Вставка данных в варианты ответов
+INSERT INTO answer (id, answer) VALUES (1, 'Рим');
+INSERT INTO answer (id, answer) VALUES (2, 'Венеция');
+INSERT INTO answer (id, answer) VALUES (3, 'Казань');
+INSERT INTO answer (id, answer) VALUES (4, 'Москва');
